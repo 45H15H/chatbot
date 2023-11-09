e594c867bb1d6a5967e283a33843362de46dbd65
Ashish Singh <ashish1qwerty1@gmail.com>
2023-11-09 23:40:44 +0530

    Add parameters

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import os
+import time
 
 # App title
 st.set_page_config(
@@ -16,7 +17,8 @@ with st.sidebar:
         st.warning('Please enter your credentials!', icon = '⚠️')
     else:
         st.success("Proceed to entering your prompt message!", icon = '✅')
-    # os.environ['OPENAI_API_KEY'] = api_key
+        st.toast('Refreshed')
+        time.sleep(0.5)
 
     st.subheader('Models and Parameters')
 
@@ -26,16 +28,16 @@ with st.sidebar:
         index = None, 
         key = 'selected_model', 
         placeholder='select a model', 
-        help = "A set of models that improve on GPT-3 and can understand as well as generate natural language or code"
-        )
+        help = "A set of models that improve on GPT-3 and can understand as well as generate natural language or code",
+        disabled=not api_key)
 
-    temperature = st.sidebar.slider(':blue[Temperature]', min_value=0.0, max_value=2.0, value = 0.0, step = 0.01, help = 'Controls the randomness of the model\'s output.')
+    temperature = st.sidebar.slider(':blue[Temperature]', min_value=0.0, max_value=2.0, value = 0.0, step = 0.01, help = 'Controls the randomness of the model\'s output.', disabled=not api_key)
     
-    max_length = st.sidebar.slider(':blue[Maximum Length]', min_value=32, max_value=128, value=120, step=8, help = 'Controls the maximum length of the model\'s response.')
+    max_length = st.sidebar.slider(':blue[Maximum Length]', min_value=100, max_value=2000, value=500, step=10, help = 'Controls the maximum length of the model\'s response.', disabled=not api_key)
 
-    independent_completions = st.sidebar.text_input(":blue[Independent Completions]", value=1, help = 'Number of independent completions to generate from the same prompt.')
+    # independent_completions = st.sidebar.number_input(":blue[Independent Completions]", value=1, help = 'Number of independent completions to generate from the same prompt.', disabled=not api_key)
 
-    stream = st.sidebar.checkbox(label='Stream Output', help = 'Return partial results as they become available, instead of waiting until the computation is done.')
+    stream = st.sidebar.toggle(label=':blue[Stream Output]', help = 'Return partial results as they become available, instead of waiting until the computation is done.', disabled=not api_key)
 
 # Store LLM generated responses
 if "message" not in st.session_state.keys():
@@ -66,28 +68,34 @@ def generate_response_gpt_turbo(message):
         messages = [{"role": "user",
                      "content": prompt}],
         temperature = temperature,
-        stream = True
+        max_tokens=max_length,
+        # n = independent_completions,
+        stream = stream
     )
-    response = ""
-    for part in chat_response:
-        response += part.choices[0].delta.content or ""
-    return response
+    if stream:
+        response = ""
+        for part in chat_response:
+            response += part.choices[0].delta.content or ""
+        return response
+    else:
+        return chat_response.choices[0].message.content
+    
 
 def generate_response_davinci(message):
     import openai
     completion = openai.completions.create(
-    model=selected_model,
-    prompt = prompt,
-    max_tokens=500
-    # stream=True
-    # )
-    # response = ""
-    # for part in completion:
-    #     response += part.choices[0].text or ""
-    # return response
+        model=selected_model,
+        prompt = prompt,
+        max_tokens=max_length,
+        stream = stream
     )
-
-    return completion.choices[0].text
+    if stream:
+        response = ""
+        for part in completion:
+            response += part.choices[0].text or ""
+        return response
+    else:
+        return completion.choices[0].text
 
 # user-provided prompt message
 if prompt := st.chat_input(disabled = not api_key):
